56c0c3349d5aeaa257d7e5a7e0320873cff0b5c6
Pedrohero13 <pedrogadote@gmail.com>
2022-05-24 14:29:18 -0500

    firts commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import { useQuery, gql } from "@apollo/client";
+
+import { useState } from 'react';
+
+const FILMS_QUERY = gql`
+{
+  champions{
+    _id
+    name
+    rarity
+    faction
+    rating
+    type
+    element
+    stats{
+      health
+      attack
+      defense
+      criticalRate
+      criticalDamage
+      speed
+      resistance
+      accuracy
+    }
+    image
+  }
+}
+`;
 function App() {
+  const { data, loading, error } = useQuery(FILMS_QUERY);
+
+  if (loading) return "Loading...";
+  if (error) return <pre>{error.message}</pre>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <h1 className=" bg-dark text-center text-danger">Champions</h1>
+      <div class="row row-cols-1 row-cols-md-6 g-4 bg-dark">
+      {data.champions.map((champion) => (
+            <div class="col border-champion-legendary">
+              <div class="card h-100 bg-dark ">
+              
+                <img src={champion.image} className="mx-auto d-block " alt={champion.name} width="140px" height="182px"/>
+                <div class="card-body">
+                  <h5 class="text-light card-title text-center">{champion.name}</h5>
+                  <p class="text-light card-text text-center">{champion.rarity}</p>
+                </div>
+                
+              </div>
+            </div>
+            
+        )
+        )}
+        
+      
+      </div>
+      
     </div>
-  );
+    
+    
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const client = new ApolloClient({
+  uri: "https://champions-api-uv.herokuapp.com/graphql",
+  cache: new InMemoryCache()
+});
 root.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+  </ApolloProvider>,
   </React.StrictMode>
 );
 
